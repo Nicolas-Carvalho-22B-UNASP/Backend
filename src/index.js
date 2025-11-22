b81960da59eb1995b3be2807147d119e838bf275
Nicolas Carvalho <nickcarva31@gmail.com>
2025-11-22 15:05:55 -0300

    4 Etapa

--- src/index.js
+++ src/index.js
@@ -24,6 +24,30 @@ app.get("/", (request, response) => {
   });
 });
 
+app.post("/login", (request, response) => {
+  const { email, password } = request.body.user;
+
+  const selectCommand = `
+  SELECT *
+  FROM nicolascarvalho_22tb
+  WHERE email = ?
+  `;
+
+  database.query(selectCommand, [email], (error, user) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    if (user.length === 0 || user[0].password !== password) {
+      response.json({ message: "Usuário ou senha incorretos!" });
+      return;
+    }
+
+    response.json ({ id: user[0].id, name: user[0].name });
+  });
+});
+
 app.post("/cadastrar", (request, response) => {
   const { user } = request.body;
   console.log(user);
